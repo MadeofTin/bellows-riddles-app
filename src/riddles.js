e678c9f9d6cb29f9c7e931308d5f310fda741ecd
Paul Carduner <paul@carduner.net>
2014-11-01 18:22:55 -0700

    switch to knuth-shuffle module

--- src/riddles.js
+++ src/riddles.js
@@ -7,24 +7,7 @@ var React = require('react');
 
 require('./styles.css')
 
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
+var shuffle = require('knuth-shuffle').knuthShuffle
 
 var StartupScreen = React.createClass({
   render: function() {
